2055f317a892cff8fe7511d2b72a57d715a77bd3
Jelte Dirks <j.dirks@outlook.com>
2022-12-21 22:02:36 +0100

    add structure for commit; print the first 6 characters of a line in the log
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::process::Command;
+
+mod structures {
+    pub struct Commit {
+        pub author: String,
+        pub date: u128,
+    }
+
+    impl Commit {
+        fn new() -> Commit {
+            return Commit {
+                author: String::from(""),
+                date: 0
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    match std::env::consts::FAMILY {
+        "unix" => {
+            println!("unix style");
+            let output = Command::new("sh")
+                .args(["-c", "git log -p"])
+                .output()
+                .expect("nuts");
+            println!("number of bytes in the output: {}", output.stdout.len());
+            for line in output.stdout.as_slice().split(|&byte: &u8| byte == 10 as u8) {
+                println!("{:?}", line);
+                let x = String::from(std::str::from_utf8(line).unwrap());
+                let commit: structures::Commit;
+                if let Some(substr) = x.get(0..6) {
+                    println!("{}", substr);
+                } else {
+                    println!("too short")
+                }
+            }
+        },
+        "windows" => {
+            println!("not supported yet")
+        }
+        _ => {
+            println!("wtf is even that")
+        }
+    }
 }
