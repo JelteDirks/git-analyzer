98c969f4c72ce035eefe06a9f5832cf3a8809d9c
Jelte Dirks <j.dirks@outlook.com>
2022-12-29 21:30:43 +0100

    use slice comparison and rework commit creation
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use std::process::Command;
 use structures::Commit;
 
+const COMMIT_LINE: &[u8; 6] = &[99, 111, 109, 109, 105, 116];
+
 mod structures {
     pub struct Commit {
         pub author: String,
         pub date: u128,
-        pub hash: Vec<u8>,
+        pub hash: String,
     }
 
     impl Commit {
-        pub fn from_hashline(hasline: &str) -> Commit {
+        pub fn new_from_all(author_line: &[u8], date_line: &[u8],
+            hash_line:&[u8]) -> Commit {
+
             return Commit {
                 author: String::from(""),
                 date: 0,
-                hash: vec!(),
+                hash: String::from(""),
             }
         }
     }
@@ -21,22 +25,29 @@ mod structures {
 
 fn main() {
     let mut commits: Vec<Commit> = vec!();
+    let mut commit_header_found: bool = false;
     match std::env::consts::FAMILY {
         "unix" => {
-            println!("unix style");
+
             let output = Command::new("sh")
                 .args(["-c", "git log -p"])
                 .output()
                 .expect("nuts");
-            println!("number of bytes in the output: {}", output.stdout.len());
-            for line in output.stdout.as_slice().split(|&byte: &u8| byte == 10 as u8) {
-                let x = String::from(std::str::from_utf8(line).unwrap());
-                if let Some(substr) = x.get(0..6) {
-                    if substr == "commit" {
-                        commits.push(Commit::from_hashline(substr));
-                    }
+
+            let mut lines = output.stdout.as_slice()
+                .split(|&byte: &u8| byte == 10 as u8)
+                .peekable();
+
+            while lines.peek() != None {
+                let line = lines.next().unwrap();
+                let commit_text = line.get(0..6).unwrap_or(&[]);
+                if commit_text == COMMIT_LINE {
+                    println!("{:?}", String::from_utf8(line.to_vec()));
+                    println!("author {:?}", String::from_utf8(lines.next().unwrap().to_vec()));
+                    println!("date {:?}", String::from_utf8(lines.next().unwrap().to_vec()));
                 }
             }
+
             println!("number of commits {:?}", commits.len());
         },
         "windows" => {
