46a1ab3ea5415e0aef79dea1d44325af71602be3
Jelte Dirks <j.dirks@outlook.com>
2023-03-09 16:28:44 +0100

    remove obsolete struct members and create a from ref fn that creates a new analytic from a referenced analytic
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/structures/analytics.rs
+++ src/structures/analytics.rs
@@ -3,22 +3,31 @@ pub struct Analytic {
     pub extension: Option<String>,
     pub additions: u32,
     pub deletions: u32,
-    pub author: Option<String>,
-    pub hash: Option<String>,
-    pub min_line: Option<String>,
-    pub plus_line: Option<String>,
 }
 
 impl Analytic {
     pub fn default() -> Analytic {
         return Analytic {
-            author: None,
             extension: None,
             additions: 0,
             deletions: 0,
-            hash: None,
-            min_line: None,
-            plus_line: None,
         };
+    }
+
+    pub fn from_ref(a: &Analytic) -> Analytic {
+        let mut default = Analytic::default();
+
+        default.extension = match &a.extension {
+            Some(s) => Some(String::from(s)),
+            None => None,
+        };
+
+        default.additions = a.additions;
+        default.deletions = a.deletions;
+
+        dbg!(a);
+        dbg!(&default);
+
+        return default;
     }
 }
