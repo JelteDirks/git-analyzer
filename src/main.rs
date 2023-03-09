c9ae8475ccd16e655d230630a5d2b6730aba2daf
Jelte Dirks <j.dirks@outlook.com>
2023-03-09 14:56:51 +0100

    create a settings struct for the analyzing of one directory, now with owned string but add lifetime for possibly &str
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::cli::args::Args;
 use clap::Parser;
 use std::{
     io::{stderr, BufWriter, Write},
-    path::{Path, PathBuf},
+    path::PathBuf, sync::{Mutex, Arc},
 };
 use structures::analytics::Analytic;
 use utils::{output::produce_output, settings::Settings};
@@ -22,6 +22,7 @@ fn main() {
     let mut err_handle = BufWriter::new(stderr());
 
     let mut analytics_list: Vec<Analytic> = Vec::with_capacity(1_000);
+    let mut arc_list = Arc::new(Mutex::new(analytics_list));
 
     dbg!(&settings);
 
@@ -37,14 +38,16 @@ fn main() {
             continue;
         }
 
-        let path_ref: PathBuf = entry.unwrap().path().to_path_buf();
+        let path_buf: PathBuf = entry.unwrap().path().to_path_buf();
 
-        if !path_ref.is_dir() {
+        if !path_buf.is_dir() {
             continue;
         }
 
+        let an_set = AnalyzeSettings::build(path_buf, settings.command.clone(), Arc::clone(&arc_list));
+
         let t_handle = thread::spawn(move || {
-            dbg!(path_ref);
+            dbg!(an_set);
         });
 
         thread_handles.push(t_handle);
@@ -54,7 +57,22 @@ fn main() {
         t_handle.join().unwrap();
     }
 
-    produce_output(analytics_list);
-
     err_handle.flush().unwrap();
+}
+
+#[derive(Debug)]
+struct AnalyzeSettings {
+    path: PathBuf,
+    command: String,
+    list: Arc<Mutex<Vec<Analytic>>>,
+}
+
+impl AnalyzeSettings {
+    fn build(path: PathBuf, command: String, list: Arc<Mutex<Vec<Analytic>>>) -> Self {
+        return AnalyzeSettings {
+            path,
+            command,
+            list,
+        }
+    }
 }
